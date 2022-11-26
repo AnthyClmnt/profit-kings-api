3ad3594a83eb6eb624a943204ff21c044a4b1219
Anthony Clermont (UG) <c0033688@newcastle.ac.uk>
2022-11-26 12:05:08 +0000

    Included cross-origin access

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
 from db import crud, models, schemas
 from db.database import SessionLocal, engine
@@ -7,6 +11,18 @@ from db.database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Dependency
@@ -16,6 +32,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+@app.get("/", response_model=list[schemas.User])
+def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
 
 @app.post("/users/", response_model=schemas.User)
@@ -35,6 +57,13 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.get("/user/{user_email}", response_model=schemas.User)
+def read_user_email(user_email: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user_email)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
